da197216333e6bb6a89dd7e8e6b24e72773151ca
ennmax <shayaan.qureshi@ontariotechu.net>
2025-11-21 22:53:54 -0500

    Added delete confirmation in /public/Script/app.js and CSS styling

--- CRUD & Authentication/WebProject/public/Script/app.js
+++ CRUD & Authentication/WebProject/public/Script/app.js
@@ -1,8 +1,13 @@
-//IIFI
-// Immediately invoed function expression
-(function(){
-    function start(){
-        console.log("App started...")
-    }
-    window.addEventListener("load",start);
+// IIFE
+(function () {
+  function start() {
+    console.log('BookTrack app started');
+  }
+
+  // Delete confirmation helper
+  window.confirmDelete = function (itemName) {
+    return confirm('Are you sure you want to delete "' + itemName + '"?');
+  };
+
+  window.addEventListener('load', start);
 })();
